85b4c58596d70871d4b929243029caa7fd996d8d
zahramarri <zahramarri.dev@gmail.com>
2022-06-20 17:17:32 +0430

    set onclick listener for buttons 0 to 9

--- app/src/main/java/com/learning/hwork7_1/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_1/MainActivity.kt
@@ -6,6 +6,7 @@ import com.learning.hwork7_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private var input: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -13,6 +14,37 @@ class MainActivity : AppCompatActivity() {
 
         setDivisionSign()
         setMultiplicationSign()
+
+        binding.buttonZero.setOnClickListener {
+            input += binding.buttonZero.text
+        }
+        binding.buttonOne.setOnClickListener {
+            input += binding.buttonOne.text
+        }
+        binding.buttonTwo.setOnClickListener {
+            input += binding.buttonTwo.text
+        }
+        binding.buttonThree.setOnClickListener {
+            input += binding.buttonThree.text
+        }
+        binding.buttonFour.setOnClickListener {
+            input += binding.buttonFour.text
+        }
+        binding.buttonFive.setOnClickListener {
+            input += binding.buttonFive.text
+        }
+        binding.buttonSix.setOnClickListener {
+            input += binding.buttonSix.text
+        }
+        binding.buttonSeven.setOnClickListener {
+            input += binding.buttonSeven.text
+        }
+        binding.buttonEight.setOnClickListener {
+            input += binding.buttonEight.text
+        }
+        binding.buttonNine.setOnClickListener {
+            input += binding.buttonNine.text
+        }
     }
 
     private fun setDivisionSign() {
