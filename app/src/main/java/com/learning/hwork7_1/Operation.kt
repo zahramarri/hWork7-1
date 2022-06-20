1067af53198bf63dfc8fd7159c915f4514e987bf
zahramarri <zahramarri.dev@gmail.com>
2022-06-20 17:06:30 +0430

    implement class operation

--- app/src/main/java/com/learning/hwork7_1/Operation.kt
+++ app/src/main/java/com/learning/hwork7_1/Operation.kt
@@ -1,4 +1,25 @@
 package com.learning.hwork7_1
 
 class Operation {
+    private var memory: Double = 0.0
+
+    fun add(a: Double = memory, b: Double): Double {
+        memory = a + b
+        return memory
+    }
+
+    fun submit(a: Double = memory, b: Double): Double {
+        memory = a - b
+        return memory
+    }
+
+    fun multiply(a: Double = memory, b: Double): Double {
+        memory = a * b
+        return memory
+    }
+
+    fun divide(a: Double = memory, b: Double): Double {
+        memory = a / b
+        return memory
+    }
 }
