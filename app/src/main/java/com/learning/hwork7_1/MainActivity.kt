99a8a6bbdcd2705297e6588678126e0a7c10b1e2
zahramarri <zahramarri.dev@gmail.com>
2022-06-21 00:09:14 +0430

    revise definition of function handle digits and call it when each digit button is clicked

--- app/src/main/java/com/learning/hwork7_1/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_1/MainActivity.kt
@@ -7,10 +7,13 @@ import com.learning.hwork7_1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private var input: String = ""
+    private var a: Double = 0.0
+    private var b: Double = 0.0
     private val divisionSign = "\u00F7"
     private val multiplicationSign = "\u00D7"
     private val digits1to9: Array<String> = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
     private val operators: Array<String> = arrayOf("+", "-", "\u00F7", "\u00D7")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,34 +23,44 @@ class MainActivity : AppCompatActivity() {
         setMultiplicationSign()
 
         binding.buttonZero.setOnClickListener {
-            input += binding.buttonZero.text
+            handleDigits0to9Input(binding.buttonZero.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonOne.setOnClickListener {
-            input += binding.buttonOne.text
+            handleDigits0to9Input(binding.buttonOne.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonTwo.setOnClickListener {
-            input += binding.buttonTwo.text
+            handleDigits0to9Input(binding.buttonTwo.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonThree.setOnClickListener {
-            input += binding.buttonThree.text
+            handleDigits0to9Input(binding.buttonThree.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonFour.setOnClickListener {
-            input += binding.buttonFour.text
+            handleDigits0to9Input(binding.buttonFour.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonFive.setOnClickListener {
-            input += binding.buttonFive.text
+            handleDigits0to9Input(binding.buttonFive.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonSix.setOnClickListener {
-            input += binding.buttonSix.text
+            handleDigits0to9Input(binding.buttonSix.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonSeven.setOnClickListener {
-            input += binding.buttonSeven.text
+            handleDigits0to9Input(binding.buttonSeven.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonEight.setOnClickListener {
-            input += binding.buttonEight.text
+            handleDigits0to9Input(binding.buttonEight.text.toString())
+            binding.resultOfOperations.text = input
         }
         binding.buttonNine.setOnClickListener {
-            input += binding.buttonNine.text
+            handleDigits0to9Input(binding.buttonNine.text.toString())
+            binding.resultOfOperations.text = input
         }
     }
 
@@ -67,15 +80,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun handleDigits0to9Input(newInput: Char) {
-        if (input.last().toString() == "0" && (input.length == 1 || input[input.indexOf("0") - 1].toString() in operators)) {
-        input.replace(input.last(), newInput)
+    private fun handleDigits0to9Input(newInput: String) {
+        if (input.lastOrNull()
+                .toString() == "0" && (input.length == 1 || input[input.indexOf("0") - 1].toString() in operators)
+        ) {
+            input = input.replace(input.last(), newInput.first())
+        } else {
+            input += newInput
         }
     }
 
     private fun handleOperatorInput(newInput: String) {
-        if (((newInput == "-") && (input.last().toString() == "\u00F7" || input.last().toString() == "\u00D7")) ||
-            input.last().toString() !in operators) {
+        if (((newInput == "-") && (input.last().toString() == "\u00F7" || input.last()
+                .toString() == "\u00D7")) ||
+            input.last().toString() !in operators
+        ) {
             input += newInput
         }
     }
