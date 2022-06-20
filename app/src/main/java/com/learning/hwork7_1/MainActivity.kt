fd08926446e0e18c7226c6c46b39af86534ec8bc
zahramarri <zahramarri.dev@gmail.com>
2022-06-20 23:08:21 +0430

    add three functions to check validity of user inputs

--- app/src/main/java/com/learning/hwork7_1/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_1/MainActivity.kt
@@ -7,6 +7,10 @@ import com.learning.hwork7_1.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private var input: String = ""
+    private val divisionSign = "\u00F7"
+    private val multiplicationSign = "\u00D7"
+    private val digits1to9: Array<String> = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+    private val operators: Array<String> = arrayOf("+", "-", "\u00F7", "\u00D7")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -48,12 +52,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setDivisionSign() {
-        val divisionSign = "\u00F7"
         binding.buttonDivisionSign.text = divisionSign
     }
 
     private fun setMultiplicationSign() {
-        val multiplicationSign = "\u00D7"
         binding.buttonMultiplicationSign.text = multiplicationSign
+    }
+
+    private fun handleDotSignInput(newInput: String) {
+        if (newInput == ".") {
+            if (!input.contains(".")) {
+                input += newInput
+            }
+        }
+    }
+
+    private fun handleDigits0to9Input(newInput: Char) {
+        if (input.last().toString() == "0" && (input.length == 1 || input[input.indexOf("0") - 1].toString() in operators)) {
+        input.replace(input.last(), newInput)
+        }
+    }
+
+    private fun handleOperatorInput(newInput: String) {
+        if (((newInput == "-") && (input.last().toString() == "\u00F7" || input.last().toString() == "\u00D7")) ||
+            input.last().toString() !in operators) {
+            input += newInput
+        }
     }
 }
