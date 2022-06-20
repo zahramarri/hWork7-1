06cb76d1ac03d372037403413b520efd81bd86d7
zahramarri <zahramarri.dev@gmail.com>
2022-06-20 15:48:35 +0430

    set text for all buttons

--- app/src/main/java/com/learning/hwork7_1/MainActivity.kt
+++ app/src/main/java/com/learning/hwork7_1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.learning.hwork7_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.hwork7_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setDivisionSign()
+        setMultiplicationSign()
+    }
+
+    fun setDivisionSign() {
+        val divisionSign = "\u00F7"
+        binding.buttonDivisionSign.text = divisionSign
+    }
+
+    fun setMultiplicationSign() {
+        val multiplicationSign = "\u00D7"
+        binding.buttonMultiplicationSign.text = multiplicationSign
     }
 }
